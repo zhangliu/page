9252efad8a39e6812cbb825a51721c485a44231d
zhangyunnian <zhangyunnian@bytedance.com>
2021-04-05 23:46:04 +0800

    modify

--- config-overrides.js
+++ config-overrides.js
@@ -2,7 +2,7 @@ const { override } = require('customize-cra');
 module.exports = override(useLoader);
 
 function useLoader(config) {
-  if (process.env.NODE_ENV === 'production') config.output.publicPath = '/job.github.io/build/';
+  if (process.env.NODE_ENV === 'production') config.output.publicPath = '/page/build/';
   config.module.rules[1].use.push({
     loader: '@byted/class-to-css-loader',
     options: {
