8d4ffe88739afe13552d5f1bafc498e9148bd2ea
zhangyunnian <zhangyunnian@bytedance.com>
2021-04-06 00:00:35 +0800

    modify

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
+const DEFAULT_COUNT = 10;
+
 export default () => {
+  const [count, setCount] = React.useState(DEFAULT_COUNT);
+  let current = count;
+  
+  React.useEffect(() => {
+    setInterval(() => {
+      current = current - 1;
+      if (current < 0) {
+        current = DEFAULT_COUNT;
+      }
+
+      setCount(current);
+    }, 1000);
+  }, []);
+
   return (
-    <div>33333</div>
+    <div>{count}</div>
   );
 }
